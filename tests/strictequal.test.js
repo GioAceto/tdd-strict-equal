e6e8b0118c45e87614f740bb43d87b5158b59d3e
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-19 18:38:31 -0400

    add strictequal function

--- tests/strictequal.test.js
+++ tests/strictequal.test.js
@@ -3,16 +3,29 @@ const it = require('mocha').it
 const expect = require('chai').expect
 const strictEqual = require("../strictequal")
 
-describe('Strict Equal Function', () => {
-    it('returns true if the values provided are strictly equal to each other', () => {
-        // set up test data
+describe('strictEqual', () => {
+    it('should return true if the numbers are the same type and value', () => {
         let a = 10
         let b = 10
-
-        //call the function from our outside file
-        strictEqual(a, b)
-
-        // assert what should be true
-        expect(a).to.equal(b)
+        const testData = strictEqual(a, b)
+        expect(testData).to.be.true
+    })
+    it('should return false if the numbers are not the same type', () => {
+        let a = '10'
+        let b = 10
+        const testData = strictEqual(a, b)
+        expect(testData).to.be.false
+    })
+    it('should return false if the numbers are not the same value', () => {
+        let a = 9
+        let b = 10
+        const testData = strictEqual(a, b)
+        expect(testData).to.be.false
+    })
+    it('should return false if the numbers are not the same type or value', () => {
+        let a = '9'
+        let b = 10
+        const testData = strictEqual(a, b)
+        expect(testData).to.be.false
     })
 })
